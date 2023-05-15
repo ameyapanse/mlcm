0c817ff065d08087786ecee6d5f70c04d9755e15
ameyapanse <ameyapanse@github.com>
2023-05-14 17:28:03 -0700

    Writing loss

--- datautils.py
+++ datautils.py
@@ -9,45 +9,23 @@ from utils import pkl_load, pad_nan_to_target
 from scipy.io.arff import loadarff
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-def load_PAMAP2(fold, axis):
-    class_to_activity_map = {
-    1: "lying",
-    2: "sitting",
-    3: "standing",
-    4: "walking",
-    5: "running",
-    6: "cycling",
-    7: "Nordic walking",
-    9: "watching TV",
-    10: "computer work",
-    11: "car driving",
-    12: "ascending stairs",
-    13: "descending stairs",
-    16: "vacuum cleaning",
-    17: "ironing",
-    18: "folding laundry",
-    19: "house cleaning",
-    20: "playing soccer",
-    24: "rope jumping",
-    0: "other transient activities"
-    }
-    v_map = np.vectorize(lambda a: class_to_activity_map.get(a, 'no matching activity found'))
-    if not fold :
+
+def load_PAMAP2(fold, axis, label_embedding_Locations='home/ranak/ameya_ritvik/mlcm/PAMAP2_label_embeddings.npy'):
+    embeddings = np.load(label_embedding_Locations)
+    if not fold:
         loc = 'datasets/PAMAP2'
-    else :
+    else:
         loc = 'datasets/PAMAP2/' + fold
     if axis:
         loc += '/' + str(axis)
-    train_file = os.path.join(loc,"X_train.npy")
+    train_file = os.path.join(loc, "X_train.npy")
     train_labels_file = os.path.join(loc, "y_train.npy")
     test_file = os.path.join(loc, "X_test.npy")
     test_labels_file = os.path.join(loc, "y_test.npy")
     train = np.load(train_file)
     train_labels = np.load(train_labels_file)
-    train_activities = v_map(train_labels)
     test = np.load(test_file)
     test_labels = np.load(test_labels_file)
-    test_activities = v_map(test_labels)
 
     mean = np.nanmean(train)
     std = np.nanstd(train)
@@ -55,4 +33,4 @@ def load_PAMAP2(fold, axis):
     test = (test - mean) / std
 
     print(train.shape, train_labels.shape, test.shape, test_labels.shape)
-    return train, train_labels, train_activities, test, test_labels, test_activities
+    return train, train_labels, test, test_labels, embeddings
--- model.py
+++ model.py
@@ -5,18 +5,20 @@ import numpy as np
 
 
 class encoder(nn.Module):
-    def __init__(self, input_dims, h1_dims, h2_dims, h3_dims, h4_dims):
+    def __init__(self, input_dims, h1_dims, h2_dims, h3_dims, h4_dims, embeddings):
         super().__init__()
         self.input_dims = input_dims
         self.h1_dims = h1_dims
         self.h2_dims = h2_dims
         self.h3_dims = h3_dims
         self.h4_dims = h4_dims
+        self.embeddings = embeddings
         self.input_h1_fc = self.fc_layer(input_dims, h1_dims)
         self.h1_h2_fc = self.fc_layer(h1_dims, h2_dims)
         self.h2_h3_fc = self.fc_layer(h2_dims, h3_dims)
         self.h4_fc = self.fc_layer(h1_dims + h2_dims + h3_dims, h4_dims)
         self.similarity = nn.CosineSimilarity()
+
 
     def fc_layer(self, in_dims, out_dims):
         return nn.Sequential(
@@ -25,11 +27,14 @@ class encoder(nn.Module):
             nn.BatchNorm1d(out_dims)
         )
 
-    def forward(self, x, y, activity_word_embedding):
+    def forward(self, x, y):
         l1 = self.input_h1_fc(x)
         l2 = self.h1_h2_fc(l1)
         l3 = self.h2_h3_fc(l2)
         l4 = self.h4_fc(torch.cat((l1, l2, l3), dim=1))
-        sim = self.similarity(l4, activity_word_embedding)
+        sc = self.similarity(l4, self.embeddings[y])
+        return sc, l4
+
+    def loss(self, ):
 
 
